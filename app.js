b0e1e23543d34d62c58f0a572b657e884c97e422
Iman Kalyan Majumder <77956318+Iman-Kalyan-Majumder@users.noreply.github.com>
2022-04-11 09:32:01 +0530

    Update app.js
    Cleansed the input data.

--- app.js
+++ app.js
@@ -2,23 +2,29 @@
             {
                 let height=Number(document.getElementById("height").value)/100;
                 let weight=Number(document.getElementById("weight").value);
-                let BMI=(weight/(height*height)).toFixed(2);
-                document.getElementsByClassName("res")[0].style.display='block';  
-                if(BMI.toString()==="NaN")
-                    document.getElementById("result").innerHTML="Please Enter The Required Data...";
-                else if(BMI<18.5)
-                {
-                    let msg="Your BMI is "+BMI.toString()+"! You are underweight...";
-                    document.getElementById("result").innerHTML=msg;
-                }
-                else if(BMI<24.5)
-                {
-                    let msg="Your BMI is "+BMI.toString()+"! You are healthy!";
-                    document.getElementById("result").innerHTML=msg;
-                }
+                
+                if(height<=0||weight<=0)
+                    document.getElementsByClassname("res").innerHTML="Please Enter The Required Data Properly...";
                 else
                 {
-                    let msg="Your BMI is "+BMI.toString()+"! You are overweight...";
-                    document.getElementById("result").innerHTML=msg;    
-                }    
+                        let BMI=(weight/(height*height)).toFixed(2);
+                        document.getElementsByClassName("res")[0].style.display='block';  
+                        if(BMI.toString()==="NaN")
+                                    document.getElementById("result").innerHTML="Please Enter The Required Data...";
+                        else if(BMI<18.5)
+                        {
+                                    let msg="Your BMI is "+BMI.toString()+"! You are underweight...";
+                                    document.getElementById("result").innerHTML=msg;
+                        }
+                        else if(BMI<24.5)
+                        {
+                                    let msg="Your BMI is "+BMI.toString()+"! You are healthy!";
+                                    document.getElementById("result").innerHTML=msg;
+                        }
+                        else
+                        {
+                                    let msg="Your BMI is "+BMI.toString()+"! You are overweight...";
+                                    document.getElementById("result").innerHTML=msg;    
+                        }    
+                }
             }
